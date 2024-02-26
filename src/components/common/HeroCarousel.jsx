1e8766bd489b51c2caeae667e4fcefb533a7670a
Gaston Rodriguez <garodriguez@arnaldocastro.com.uy>
2024-02-25 23:35:22 -0300

    carousel height ok

--- src/components/common/HeroCarousel.jsx
+++ src/components/common/HeroCarousel.jsx
@@ -7,7 +7,7 @@ import { Autoplay, Pagination, Navigation, Scrollbar } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
-
+import "./HeroCarousel.css"
 const HeroCarousel = (data) => {
   const posts = data.data;
   return (
@@ -30,23 +30,21 @@ const HeroCarousel = (data) => {
         {posts.map((post, index) => (
           <SwiperSlide
             key={post.id}
-            className="bg-white shadow-md rounded-lg p-4 w-full md:w-10/12 lg:w-8/12"
+            className=""
           >
-            <div
-              className="bg-cover bg-no-repeat bg-center main-carousel flex items-center justify-center bg-blend-soft-light"
-              style={{
-                backgroundImage: `url(${post.featuredImage?.node.sourceUrl})`,
-                opacity: 0.5,
-              }}
-            >
-              
-              <h2 className="text-center text-2xl font-bold text-white">
-                {post.title}
-              </h2>
-              <div className="description">
-                <p>{post.author.node.name}</p>
-                <p>{post.date}</p>
-                <p>{post.categories.nodes[0].name}</p>
+            <div className="bg-cover bg-no-repeat bg-center main-carousel flex items-center justify-center bg-blend-soft-light">
+              <img src={post.featuredImage?.node.sourceUrl} alt="Image description" className="object-fill"/>
+              <div class="absolute inset-0 flex justify-center items-center">
+                    <div class="absolute inset-0 flex justify-center items-center">
+                      <h2 className="text-center text-2xl font-bold text-white">
+                      {post.title}
+                      </h2>
+                      <div className="description absolute inset-0 flex justify-center items-center">
+                          <p>{post.author.node.name}</p>
+                          <p>{post.date}</p>
+                          <p>{post.categories.nodes[0].name}</p>
+                      </div>
+                    </div>
               </div>
             </div>
           </SwiperSlide>
